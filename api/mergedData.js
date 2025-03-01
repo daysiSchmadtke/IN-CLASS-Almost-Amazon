679dc88342a09db499dd8c3ed26b42aa34182f2e
Daysi Schmadtke <daysischmadtke@gmail.com>
2025-02-28 23:30:21 -0500

    test

--- api/mergedData.js
+++ api/mergedData.js
@@ -1,1 +1,24 @@
 // for merged promises
+import { getSingleBook, deleteBook } from './bookData';
+import { getSingleAuthor, deleteSingleAuthor, getAuthorBooks } from './authorData';
+
+const deleteAuthorBooksRelationship = (firebaseKey) => new Promise((resolve, reject) => {
+  getAuthorBooks(firebaseKey).then((authorBooksArray) => {
+    const deleteBookPromises = authorBooksArray.map((book) => deleteBook(book.firebaseKey));
+
+    Promise.all(deleteBookPromises).then(() => {
+      deleteSingleAuthor(firebaseKey).then(resolve);
+    });
+  }).catch(reject);
+});
+
+const getBookDetails = (firebaseKey) => new Promise((resolve, reject) => {
+  // GET SINGLE BOOK
+  getSingleBook(firebaseKey).then((bookObject) => { // returns single book object
+    getSingleAuthor(bookObject.author_id) // we nest this promise so that we can use the book object
+      .then((authorObject) => resolve({ ...bookObject, authorObject }));
+  }).catch(reject);
+  // GET AUTHOR
+  // Create an object that has book data and an object named authorObject
+});
+export { deleteAuthorBooksRelationship, getBookDetails };
